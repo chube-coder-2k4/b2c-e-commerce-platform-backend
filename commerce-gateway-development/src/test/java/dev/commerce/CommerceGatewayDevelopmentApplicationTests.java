014601548670d95292448d0d39c0d8b8550d41d0
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-11-11 10:56:38 +0700

    commerce init folder

---
+++ commerce-gateway-development/src/test/java/dev/commerce/CommerceGatewayDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceGatewayDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
