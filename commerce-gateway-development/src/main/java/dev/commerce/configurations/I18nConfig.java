d3df28b27ae576da01a1850f9d33d8b2cf09958a
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-12-24 01:57:07 +0700

    feat: impl i18n

---
+++ commerce-gateway-development/src/main/java/dev/commerce/configurations/I18nConfig.java
@@ -0,0 +1,27 @@
+package dev.commerce.configurations;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Configuration
+public class I18nConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(java.util.Locale.ENGLISH);
+        return localeResolver;
+    }
+}
---
+++ commerce-gateway-development/src/main/java/dev/commerce/utils/MessageUtils.java
@@ -0,0 +1,16 @@
+package dev.commerce.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageUtils {
+    private final MessageSource messageSource;
+
+    public String get(String key){
+        return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+    }
+}
