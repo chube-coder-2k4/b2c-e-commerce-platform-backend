f66299765477b6db91f7aa665d1a077d13b136f3
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-01-07 10:31:47 +0700

    feat: impl page response

---
+++ commerce-gateway-development/src/main/java/dev/commerce/dtos/response/PageResponse.java
@@ -0,0 +1,10 @@
+package dev.commerce.dtos.response;
+
+import java.io.Serializable;
+
+public class PageResponse<T> implements Serializable {
+    private int pageNo;
+    private int pageSize;
+    private int totalPages;
+    private T items;
+}
